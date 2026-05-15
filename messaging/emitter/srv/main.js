46fd5c2d4227a8a59f9e8088ab91efab62e8945b
DJ Adams <qmacro@gmail.com>
2026-05-15 09:56:05 +0000

    cleanup of messaging

--- messaging/emitter/srv/main.js
+++ messaging/emitter/srv/main.js
@@ -1,10 +1,11 @@
 const cds = require('@sap/cds')
 const log = cds.log('emitter')
+
 module.exports = cds.service.impl(async function() {
   this.on('greet', async (req) => {
     const emitter = await cds.connect.to('org.qmacro.emitter.EmitterService')
     log(`emitting Greeting.Received (${req.data.greeting})`)
     await emitter.emit('Greeting.Received', { info: req.data.greeting })
-    return 'OK';
+    return 'OK'
   })
 })
